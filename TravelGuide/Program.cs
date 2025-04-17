7ef65e9a997af7d3e190dc3c758e0d44e229abec
Samraatghimire <102979518+Samraatghimire@users.noreply.github.com>
2025-04-17 11:26:38 +0545

    Update Program.cs

--- TravelGuide/Program.cs
+++ TravelGuide/Program.cs
@@ -1,41 +1,91 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TravelGuide.Data;
-using TravelGuide.Repository;
+using TravelGuide.Repository.Posts;
+using TravelGuide.Repository.User;
 using TravelGuide.Services;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
-
-
+ 
+ 
 // Add services to the container.
-
-builder.Services.AddControllers();
+ 
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TravelGuide API", Version = "v1" });
+ 
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter: Bearer {your JWT token}"
+    });
+ 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+ 
 //Registering the HttpClient
 builder.Services.AddHttpClient();
-
+ 
+//Registering the WebRootPath
+builder.WebHost.UseWebRoot("wwwroot");
+ 
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+ 
 //Registering the GooglePlacesService
 builder.Services.AddScoped<IPlacesService, GeoapifyPlacesService>();
-
+ 
 //Registering the UserRepository
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
+ 
 //Registering the JWT Token Service
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
-
-
+ 
+ 
 // Registering the EmailService
 builder.Services.AddScoped<IEmailService, EmailService>();
-
+ 
+//Registering the PostService
+builder.Services.AddScoped<IPostService, PostService>();
+ 
+//Registering the PostRepository
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+ 
 // Registering the DbContext with PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
+ 
+ 
 // Registering the JWT authentication
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options =>
@@ -51,10 +101,10 @@ builder.Services.AddAuthentication("Bearer")
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
     });
-
+ 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowLocalhost", policy =>
+    options.AddPolicy("AllowLocalhost", policy =>  // this is only for the development purpose in the production this will cause the security issue.
     {
         policy.SetIsOriginAllowed(origin =>
             new Uri(origin).Host == "localhost") 
@@ -63,17 +113,11 @@ builder.Services.AddCors(options =>
               .AllowCredentials(); 
     });
 });
-
+ 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
+ 
 // Configure the HTTP request pipeline.
-
+ 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -82,16 +126,19 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseSwagger();
+    app.UseSwagger();  // this will not be needed in the production
     app.UseSwaggerUI();
 }
-
+ 
+// Enable static files middleware
+app.UseStaticFiles();
+ 
 app.UseHttpsRedirection();
-
+ 
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors("AllowLocalhost");
-
+ 
 app.MapControllers();
-
+ 
 app.Run();
